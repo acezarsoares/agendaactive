005723a22177558bc6ba3199f74ed05fbcc2b957
cmcant <cmca_nt@msn.com>
2018-01-31 15:17:54 -0200

    Ajuste de login escola

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { LoginProvider } from '../../providers/loginprovider/loginprovider';
 
 export class Usuario{
+  escola:string;
   operador: String;
   senha: String;
 }
