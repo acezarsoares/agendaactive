4497c7cbe46bed756e7ef44731e5e6a536c0acc1
Anderson Soares <acezarsoares@gmail.com>
2018-02-01 16:37:48 -0300

    adicionando a estrutura a pasta do projeto da API

---
+++ API/API.dpr
@@ -0,0 +1,36 @@
+library API;
+
+{$R *.dres}
+
+uses
+  Winapi.ActiveX,
+  System.Win.ComObj,
+  Web.WebBroker,
+  Web.Win.ISAPIApp,
+  Web.Win.ISAPIThreadPool,
+  Data.DBXCommon,
+  Datasnap.DSSession,
+  ServerMethodsUnit1 in 'ServerMethodsUnit1.pas' {ServerMethods1: TDSServerModule},
+  ServerContainerUnit1 in 'ServerContainerUnit1.pas' {ServerContainer1: TDataModule},
+  WebModuleUnit1 in 'WebModuleUnit1.pas' {WebModule1: TWebModule};
+
+{$R *.res}
+
+exports
+  GetExtensionVersion,
+  HttpExtensionProc,
+  TerminateExtension;
+
+procedure TerminateThreads;
+begin
+  TDSSessionManager.Instance.Free;
+  Data.DBXCommon.TDBXScheduler.Instance.Free;
+end;
+
+begin
+  CoInitFlags := COINIT_MULTITHREADED;
+  Application.Initialize;
+  Application.WebModuleClass := WebModuleClass;
+  TISAPIApplication(Application).OnTerminate := TerminateThreads;
+  Application.Run;
+end.
