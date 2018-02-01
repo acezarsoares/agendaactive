fcb84abeb561906a2be5ab8d13c1b3e53c64ed5a
cmcant <cmca_nt@msn.com>
2018-02-01 09:25:41 -0200

    Ajuste login admin

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { LoginProvider } from './../../providers/loginProvider/loginprovider';
+import { TabsPage } from '../../pages/tabs/tabs';
 
 export class Usuario{
   escola:string;
@@ -28,6 +29,11 @@ export class LoginPage {
 
 
   login(){
-    this.LoginProvider.showAlert('Login', 'Acesso ao sistema garantido');
+    if(this.user.operador == 'admin' || this.user.senha=='123'){
+      this.LoginProvider.showAlert('Login', 'Acesso ao sistema garantido');
+      this.navCtrl.push('TabsPage');
+     } else {
+      this.LoginProvider.showAlert('Login', 'Acesso negado!');
+     }
   }
 }
