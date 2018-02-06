2fe636918b0788b20147ded655984b0dd7adbebf
System Administrator <root@MacBook-Pro-de-Himni.local>
2018-02-05 23:15:06 -0300

    Ajuste na forma de chamar o servidor

--- src/providers/mensagem/mensagem.ts
+++ src/providers/mensagem/mensagem.ts
@@ -5,19 +5,23 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class MensagemProvider {
 
-  private API_URL:string = 'http://10.10.10.131/API/api.dll/datasnap/rest/TServerMethods/'; 
   private data:any;
 
   constructor(public http: Http) {  
   }
 
-  getMensagem(idmobiletipomensagem){
+  public Servidor(){
+    return 'http://10.10.10.131/API/api.dll/datasnap/rest/TServerMethods/';
+  }
+
+  public getMensagem(idmobiletipomensagem){
     //if (this.data) {    
     //  return Promise.resolve(this.data);
     //}
   
-    return new Promise(resolve => {      
-      this.http.get(this.API_URL + 'Mensagem/112141/' + idmobiletipomensagem)
+    return new Promise(resolve => {   
+
+      this.http.get(this.Servidor() + 'Mensagem/112141/' + idmobiletipomensagem)
         .map(res => res.json())
         .subscribe(data => {
           this.data = data;
